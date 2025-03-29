a685c9ceb4ed94b284a71aad915ea75f6bad679a
Laugh1ngGas <boy.bogdan03@gmail.com>
2025-03-29 02:04:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import HeroSection from "./components/HeroSection.jsx";
+import FeatureSection from "./components/FeatureSection.jsx";
+import Workflow from "./components/WorkflowSection.jsx";
+import Pricing from "./components/Pricing.jsx";
+import Testimonials from "./components/Testimonials.jsx";
+import Footer from "./components/Footer.jsx";
+
+const App = () => {
+  return(<div>
+    <Navbar />
+    <div className="max-w-7xl mx-auto pt-20 px-6">
+      <HeroSection />
+      <FeatureSection />
+      <Workflow />
+      <Pricing />
+      <Testimonials />
+      <Footer />
+    </div>
+  </div>)
+}
+
+export default App
